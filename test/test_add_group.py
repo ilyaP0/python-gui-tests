4585b21b394c91595dd782b656e3fe97d627219e
Илья <ilya-31071992@mail.ru>
2019-03-31 15:31:33 +0300

    Реализован генератор тестовых данных в формате .xlsx

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,2 +1,6 @@
 def test_add_group(app):
-    pass
+    old_list = app.groups.get_group_list()
+    app.groups.add_new_group(name="dsaddasad")
+    new_list = app.groups.add_new_group(name="dsaddasad")
+    new_list.append(name="dsaddasad")
+    assert sorted(old_list) == sorted(new_list)
